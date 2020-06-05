8f94aac2b4bb5919f2a7b678ddde67f3152f3a26
Viktor Lennert <geozukunft@outlook.at>
2020-06-05 23:20:06 +0200

    implemented the delegate function to allow me to use a event handler

--- Roulette_with_Arduino/Form1.Designer.cs
+++ Roulette_with_Arduino/Form1.Designer.cs
@@ -28,20 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.serialCommToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.portToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cboPorts = new System.Windows.Forms.ToolStripComboBox();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeApplicationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblEvenUneven = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.cmdRequest = new System.Windows.Forms.Button();
+            this.lboData = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.serialCommToolStripMenuItem,
+            this.closeApplicationToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(12, 4, 0, 4);
+            this.menuStrip1.Size = new System.Drawing.Size(1562, 44);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // serialCommToolStripMenuItem
+            // 
+            this.serialCommToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.portToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.serialCommToolStripMenuItem.Name = "serialCommToolStripMenuItem";
+            this.serialCommToolStripMenuItem.Size = new System.Drawing.Size(163, 36);
+            this.serialCommToolStripMenuItem.Text = "Serial Comm";
+            // 
+            // portToolStripMenuItem
+            // 
+            this.portToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cboPorts,
+            this.openToolStripMenuItem});
+            this.portToolStripMenuItem.Name = "portToolStripMenuItem";
+            this.portToolStripMenuItem.Size = new System.Drawing.Size(172, 38);
+            this.portToolStripMenuItem.Text = "Port";
+            // 
+            // cboPorts
+            // 
+            this.cboPorts.Name = "cboPorts";
+            this.cboPorts.Size = new System.Drawing.Size(121, 40);
+            this.cboPorts.Click += new System.EventHandler(this.cboPorts_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(205, 38);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(172, 38);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // closeApplicationToolStripMenuItem
+            // 
+            this.closeApplicationToolStripMenuItem.Name = "closeApplicationToolStripMenuItem";
+            this.closeApplicationToolStripMenuItem.Size = new System.Drawing.Size(212, 36);
+            this.closeApplicationToolStripMenuItem.Text = "Close Application";
+            this.closeApplicationToolStripMenuItem.Click += new System.EventHandler(this.closeApplicationToolStripMenuItem_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(77, 225);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(70, 25);
+            this.lblResult.TabIndex = 9;
+            this.lblResult.Text = "label1";
+            // 
+            // lblEvenUneven
+            // 
+            this.lblEvenUneven.AutoSize = true;
+            this.lblEvenUneven.Location = new System.Drawing.Point(154, 224);
+            this.lblEvenUneven.Name = "lblEvenUneven";
+            this.lblEvenUneven.Size = new System.Drawing.Size(70, 25);
+            this.lblEvenUneven.TabIndex = 10;
+            this.lblEvenUneven.Text = "label1";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(82, 78);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 31);
+            this.txtInput.TabIndex = 12;
+            // 
+            // cmdRequest
+            // 
+            this.cmdRequest.Location = new System.Drawing.Point(247, 78);
+            this.cmdRequest.Name = "cmdRequest";
+            this.cmdRequest.Size = new System.Drawing.Size(185, 55);
+            this.cmdRequest.TabIndex = 13;
+            this.cmdRequest.Text = "Rièn n va plus";
+            this.cmdRequest.UseVisualStyleBackColor = true;
+            this.cmdRequest.Click += new System.EventHandler(this.cmdRequest_Click);
+            // 
+            // lboData
+            // 
+            this.lboData.FormattingEnabled = true;
+            this.lboData.ItemHeight = 25;
+            this.lboData.Location = new System.Drawing.Point(479, 78);
+            this.lboData.Name = "lboData";
+            this.lboData.Size = new System.Drawing.Size(228, 429);
+            this.lboData.TabIndex = 14;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1562, 832);
+            this.Controls.Add(this.lboData);
+            this.Controls.Add(this.cmdRequest);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblEvenUneven);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.menuStrip1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem serialCommToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem portToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox cboPorts;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeApplicationToolStripMenuItem;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblEvenUneven;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button cmdRequest;
+        private System.Windows.Forms.ListBox lboData;
     }
 }
 
--- Roulette_with_Arduino/Form1.cs
+++ Roulette_with_Arduino/Form1.cs
@@ -7,14 +7,137 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
 
 namespace Roulette_with_Arduino
 {
     public partial class Form1 : Form
     {
+        public delegate void AddDataDelegate(String myString);
+        public AddDataDelegate Delegate1;
+        public AddDataDelegate Delegate2;
+        public AddDataDelegate Delegate3;
+        int[] data; 
+
+
         public Form1()
         {
             InitializeComponent();
+
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);
+            this.Delegate1 = new AddDataDelegate(AddDataMethodNumber);
+            this.Delegate2 = new AddDataDelegate(AddDataMethodEven);
+            this.Delegate3 = new AddDataDelegate(AddDatatoFileAndLBO);
+        }
+
+        private static void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            //data received on serial port is asssigned to "indata" string
+            //Console.WriteLine("Received data:");
+            //Console.Write(indata);
+        }
+
+        public void AddDatatoFileAndLBO(String myString)
+        {
+            lboData.Items.Add(myString);
+
+        }
+
+        public void AddDataMethodNumber(String myString)
+        {
+            lblResult.Text = myString;
+        }
+
+        public void AddDataMethodEven(String myString)
+        {
+            lblEvenUneven.Text = myString;
+        }
+
+        private void mySerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+
+            string[] splitLine = indata.Split(';');
+
+            int values = data.Count();
+            data[values] = Convert.ToInt32(splitLine[0]);
+
+            lblResult.Invoke(this.Delegate1, new Object[] { splitLine[0] });
+            lblEvenUneven.Invoke(this.Delegate2, new Object[] { splitLine[1] });
+            lboData.Invoke(this.Delegate3, new object[] { splitLine[0] });
+        }
+
+        private void ListCom()
+        {
+            //Write all available ports into String
+            string[] ports = SerialPort.GetPortNames();
+
+            //Display available Ports
+            foreach (string port in ports)
+            {
+                cboPorts.Items.Add(port);
+            }
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (cboPorts.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select COM-POrt");
+            }
+            else
+            {
+                serialPort1.PortName = cboPorts.SelectedItem.ToString();
+                if (!serialPort1.IsOpen)
+                {
+                    try
+                    {
+                        serialPort1.Open();
+                        MessageBox.Show("Port opened");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("A error occurred!");
+                        MessageBox.Show(ex.ToString());
+                    }
+                }
+            }
+        }
+
+        private void closeApplicationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cboPorts_Click(object sender, EventArgs e)
+        {
+            ListCom(); //List Ports in Menu
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                MessageBox.Show("Port closed");
+            }
+            else
+            {
+                MessageBox.Show("Port not open");
+            }
+        }
+
+        private void cmdRequest_Click(object sender, EventArgs e)
+        {
+            serialPort1.WriteLine("1");
         }
     }
+
+
+
 }
